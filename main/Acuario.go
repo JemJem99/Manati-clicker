acea351eabce3d3c1d3af03fa487db12604dc33f
JemJem99 <123089570+JemJem99@users.noreply.github.com>
2025-01-16 18:47:06 +0100

    sd

---
+++ main/Acuario.go
@@ -0,0 +1,16 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"aquarium\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/imatge.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 0.2
+    y: 0.2
+    z: 0.2
+  }
+}
